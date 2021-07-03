efb375a3274b570a09d13413d6cd9dc2b867cb93
himanshu1066 <75839286+himanshu1066@users.noreply.github.com>
2021-07-03 22:54:35 +0530

    Add files via upload

---
+++ leap year.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+main()
+{
+int year;
+printf("enter the year you want to check");
+scanf("%d",&year);
+if(year%400==0)
+{
+printf("%d is leap year \n",year);
+
+}
+else if(year%100==0)
+{
+printf("%d is not a leap year \n",year);
+
+}
+else if(year%4==0)
+{
+printf("%d is a leap year\n",year);
+
+}
+else
+{
+printf("%d is not a leap year\n",year);
+}
+
+}
+
+
