5ee8cad607025f85e24d066be134815251f45f83
himanshu1066 <75839286+himanshu1066@users.noreply.github.com>
2021-07-03 22:58:14 +0530

    Add files via upload

---
+++ all odd number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+main()
+{
+int i,n;
+printf("enter a number\n");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+if(i%2==1)
+printf("%d\n",i);
+else
+continue;
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
