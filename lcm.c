668b02525e9c2199a3bfae2d9181c6c0f5e0b145
himanshu1066 <75839286+himanshu1066@users.noreply.github.com>
2021-07-03 23:02:00 +0530

    Add files via upload

---
+++ lcm.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+
+
+int a,b,l;
+printf("enter two number");
+scanf("%d%d",&a,&b);
+for(l=a>b?a:b;l<=a*b;l=l+(a>b?a:b))
+if(l%a==0&&l%b==0)
+break;
+printf("lcm is %d",l);
+
+}
+
