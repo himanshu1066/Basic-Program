32f60ad3993d1cfda001e55ec0ffc7615c8f8b2d
himanshu1066 <75839286+himanshu1066@users.noreply.github.com>
2021-07-03 23:06:21 +0530

    Add files via upload

---
+++ array sum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int arr[10];
+int i,sum=0;
+printf("enter 10 number of element");
+for(i=1;i<=10;i++)
+scanf("%d",&arr[i]);
+{
+for(i=1;i<=10;i++)
+sum=sum+arr[i];
+
+
+}
+printf("the sum of array is :%d\n",sum);
+
+
+
+
+
+
+
+}
