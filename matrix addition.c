49d8fc1d5ac0c25b7d46540923b46bcb7fcc92c8
himanshu1066 <75839286+himanshu1066@users.noreply.github.com>
2021-07-03 23:02:38 +0530

    Add files via upload

---
+++ matrix addition.c
@@ -0,0 +1,24 @@
+void main()
+{
+int a[3][3],b[3][3],c[3][3],i,j;
+printf("enter 9 number of first matrix");
+for(i=0;i<=2;i++)
+for(j=0;j<=2;j++)
+scanf("%d",&a[i][j]);
+
+printf("enter 9 number of first matrix");
+for(i=0;i<=2;i++)
+for(j=0;j<=2;j++)
+scanf("%d",&b[i][j]);
+printf("sum of matrics is \n");
+for(i=0;i<=2;i++)
+{
+for(j=0;j<=2;j++)
+{
+c[i][j]=a[i][j]+b[i][j];
+printf("%d",c[i][j]);
+
+}
+printf("\n");
+}
+}
